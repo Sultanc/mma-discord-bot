bd58f4ef437ba66f8eb42b23026cbdcbb203c9bd
Sultan <Soltan6@hotmail.fr>
2021-04-28 06:59:19 +0300

    mma-puppteer

---
+++ README.md
@@ -0,0 +1,32 @@
+Simple Discord bot that gets a screenshot of the next MMA fight card for UFC, Bellator and PFL from [ESPN](https://www.espn.com/mma/fightcenter)
+
+
+------------------------------------
+**Description:**
+
+Simple bot that does simple task.
+
+------------------------------------
+
+**Usage:**
+
+**!ufc :** gets UFC next fight card
+
+**!bellator :** gets Bellator next fight card
+
+**!pfl :** gets PFL next fight card
+
+------------------------------------
+
+this bot gets to ESPN website via puppeteer headless browser
+
+------------------------------------
+
+**Dependencies :**
+
+[Discord.js](https://discord.js.org/#/)
+
+[Puppeteer](https://www.npmjs.com/package/puppeteer)
+
+
+
---
+++ mma-puppeteer.js
@@ -0,0 +1,52 @@
+const Discord = require('discord.js');
+const puppeteer = require('puppeteer');
+const {prefix, token} = require('./config.json');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+let url;
+//css path  
+const cookie = '#onetrust-accept-btn-handler'//cookie css
+const cardSelector = 'html body div#espnfitt div#DataWrapper div#fitt-analytics div.bp-mobileTablet.bp-tablet.bp-mobileMDPlus.bp-mobileLGPlus.bp-tabletPlus.no-touch div#fittPageContainer div div.PageLayout.page-container.cf.PageLayout--tablet.PageLayout--93'
+
+client.on('message', message => {
+    if (message.content === `${prefix}ufc`) {
+        url = 'https://www.espn.com/mma/fightcenter/_/league/ufc'
+        getCard()
+    }
+    else if (message.content === `${prefix}bellator`) {
+        url = 'https://www.espn.com/mma/fightcenter/_/league/bellator'
+        getCard()
+    }
+    else if (message.content === `${prefix}pfl`) {
+        url = 'https://www.espn.com/mma/fightcenter/_/league/pfl'
+        getCard()
+    }
+
+        function getCard() {
+        (async () => {
+            const browser = await puppeteer.launch({args: ['--no-sandbox']});
+            console.log('browser launched')
+            const page = await browser.newPage();
+            await page.goto(url);
+            await Promise.all([
+                await page.waitForSelector(cookie),
+                await page.click(cookie) //accept cookie
+              ]);
+            console.log('cookie accepted') 
+            await page.waitForTimeout(3000) //3 sec wait
+            const cardElement = await page.$(cardSelector) //select css path
+            await cardElement.screenshot({path: `./images/card.png`}); //card screenshot
+            const attachment = new Discord.MessageAttachment(`./images/card.png`);
+            message.channel.send(`${message.author}`, attachment);
+            console.log(`card sent`)
+            await browser.close();
+            console.log('browser closed')
+        })();}
+    }
+);
+
+client.login(token);
